04702bd7198954050d5a48724166b03cfdecad14
Martín Braga <martin.braga252@gmail.com>
2022-06-20 14:44:28 -0300

    Se añade annotations y atributos a CharacterDE

--- src/main/java/com/palermo/goblins/model/de/CharacterDE.java
+++ src/main/java/com/palermo/goblins/model/de/CharacterDE.java
@@ -1,4 +1,40 @@
 package com.palermo.goblins.model.de;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "CHARACTER")
 public class CharacterDE {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "ATK")
+    private int atk;
+
+    @Column(name = "HP")
+    private int hp;
+
+    @Column(name = "DEF")
+    private int def;
+
+    @Column(name = "SPD")
+    private int spd;
+
+    @Column(name = "SEXMETER")
+    private int sexMeter;
 }
